6c2d2fbb95db6dbeea8b101bb0d53ef6bec131de
kumarvivekpandey <35356580+kumarvivekpandey@users.noreply.github.com>
2023-07-28 10:23:52 +0000

    io

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,29 @@ import {useEffect, useRef} from "react";
 function App() {
   const imgRef= useRef();
   const canvasRef= useRef();
+  const handleImage = async()=>{
+    const detections = await faceapi .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+    .withFaceLandmarks().withFaceExpressions();
+    console.log(detections);
+  };
   useEffect(()=>{
     const loadModels =()=>{
       Promise.all([
-        
-        faceapi.nets.faceExpressionNet.loadFromuri("/models"),
         faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.faceRecognitionNet.loadFromuri("/models"),  
-        faceapi.nets.faceLandmark68Net.loadFromuri("/models"),     
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
       ])
-      .then(console.log("its done")).catch((e)=> console.log(e));
+      .then(console.log(handleImage))
+        .catch((e)=> console.log(e));
     };
     imgRef.current && loadModels();
   },[]);
   return (
     <div className="App">
      
-        <img  ref={imgRef} src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" 
+         <img 
+           ref={imgRef} src="https://img.freepik.com/free-photo/people-taking-selfie-together-registration-day_23-2149096795.jpg" 
           width="940"
           height="650" alt="logo" />
        <canvas ref={canvasRef} width="940"
