63cee68e4651361e9ffce05e84bc5ee6afc60a9a
vivek kumar <vkpvivek1998@gmail.com>
2023-07-28 12:40:13 +0530

    updated
    updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" className="App-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
